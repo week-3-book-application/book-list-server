c74c0104ff17ea9aa405df106cb286b346937bea
Carl Olson <colson10@gmail.com>
2018-02-26 16:06:23 -0800

    initial setup of server

---
+++ server.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const express = require('express');
+const cors = require('cors');
+const pg = require('pg');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+const CLIENT_URL = process.env.CLIENT_URL;
+
+const client = new pg.Client(process.env.DATABASE_URL);
+client.connect();
+
+app.use(cors());
+
+app.get('/', (req, res) => res.send('Testing 1, 2, 3'));
+
+app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
