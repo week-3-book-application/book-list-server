c00b69691fdd92f4e7f4524c65ac142d691ba010
Kris Sakarias <ksakarias@gmail.com>
2018-02-27 12:54:44 -0800

    working on get reqest

--- server.js
+++ server.js
@@ -7,13 +7,20 @@ const pg = require('pg');
 const app = express();
 const PORT = process.env.PORT || 3000;
 const CLIENT_URL = process.env.CLIENT_URL;
+const DATABASE_URL = process.env.DATABASE_URL;
 
-const client = new pg.Client(process.env.DATABASE_URL);
+const client = new pg.Client(DATABASE_URL);
 client.connect();
+client.on('error', err => console.error(err));
 
 app.use(cors());
 
 app.get('/', (req, res) => res.send('Testing 1, 2, 3'));
+
+// app.get(`${__API_URL__}api/v1/books`) = (request, response) => {
+//   client.query(`SELECT * FROM books;`)
+//     .then(result => response.send(result));
+// }
 
 app.get('*', (req, res) => res.redirect(CLIENT_URL));
 app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
