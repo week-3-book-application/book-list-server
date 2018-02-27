b28d61a0aabdaa821c9c7cfdd29dabc25b9cdc97
Carl Olson <colson10@gmail.com>
2018-02-26 16:50:14 -0800

    added redirect

--- server.js
+++ server.js
@@ -15,4 +15,5 @@ app.use(cors());
 
 app.get('/', (req, res) => res.send('Testing 1, 2, 3'));
 
+app.get('*', (req, res) => res.redirect(CLIENT_URL));
 app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
